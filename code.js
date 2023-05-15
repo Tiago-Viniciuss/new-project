4c4124c38c1c160668169d3de71e0f77d589876a
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-05-15 01:35:26 +0100

    primeiro commit
    Fiz o esqueleto da página, adicionei algumas imagens, arquivos CSS e JS

---
+++ code.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    let menu = document.getElementById('menu');
+    let menuComplement = document.getElementById('menuComplement')
+
+    menu.classList.toggle('active');
+    menuComplement.classList.toggle('active');
+};
